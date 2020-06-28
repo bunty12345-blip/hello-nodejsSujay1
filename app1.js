21c7e40e29ada90b8aa24562344f5c69f8a3519e
bunty12345-blip <1806081@kiit.ac.in>
2020-06-29 01:36:12 +0530

    First commit app1 json code

---
+++ app1.js
@@ -0,0 +1,14 @@
+{
+    "name": "hello",
+    "version": "2.0",
+    "description": "This is hackers world not to mess with!!!",
+    "author": "Jonathan James",
+    scripts: {
+        "start": "node app1.js"
+    }
+    "dependencies": {
+        "cookie-parser": "1.4.*",
+        "http-errors": ">=1.6.3",
+    },
+    "license": "KIIT"
+}
